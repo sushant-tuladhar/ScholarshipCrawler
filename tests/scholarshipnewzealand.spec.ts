aa7e050db489a089347dbd7f199270e245cd8184
Sushant Tuladhar <sushanttuladhar@Sushants-MacBook-Pro.local>
2024-12-28 13:03:10 +0545

    Test condition for checking if the eligibility is enabled

--- tests/scholarshipnewzealand.spec.ts
+++ tests/scholarshipnewzealand.spec.ts
@@ -1,7 +1,19 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Test for the new zealand scholarship website', () => {
-  test('Should open the website for scholarship', async ({ page }) => {
+
+  test('Should open the website for scholarship and check if eligibility test is available or not ', async ({ page }) => {
+    //Open the website
     await page.goto('https://nzscholarships.my.site.com/Scholar/s/eligibility-test');
+
+    //Check if the website is opened and get by the CSS selector
+    const closed = page.locator('xpath=//span[text()="The eligibility test and ability to create accounts is currently closed."]');
+    
+    let isEnabled = await closed.isEnabled();
+    let isVisible= await closed.isVisible();
+
+    expect(isEnabled).toBe(false);
+    expect(isVisible).toBe(false);
+    
   });
 });
